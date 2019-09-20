606e077961dd10b30e3f1407531f53e6cde5131c
ruobin.lb <ruobin.lb@alibaba-inc.com>
2019-09-20 13:27:28 +0800

    upgrade core sdk to v0.2.0

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 
 setup(  
     name = "lecoresdk",
-    version = "1.0",
+    version = "1.1",
     description = "Link IoT Edge Core SDK for Function Compute",
     license = "Apache 2.0",
   
     url = "https://help.aliyun.com/product/69083.html?spm=a2c4g.11186623.6.540.66bd71b80FSb2h",
-    packages = ['lecoresdk'],
+    packages = ['lecoresdk', 'leda_python'],
     include_package_data = True,
     platforms = "any",
     install_requires = [
